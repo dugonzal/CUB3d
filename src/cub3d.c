1baa1e2c7e5eef206659ca957df9cacfd70c72c1
dugonzal <ciclocontinuo2@gmail.com>
2023-06-26 15:35:09 +0200

    agrego header y le paso a la estrcutur

--- src/cub3d.c
+++ src/cub3d.c
@@ -1,11 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/26 15:32:36 by Dugonzal          #+#    #+#             */
+/*   Updated: 2023/06/26 15:34:17 by Dugonzal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/cub3d.h"
 
-//falta comprobar q fd exista o algo
 int cub3d(t_game *game, char **av)
 {
+  t_img	img;
+  t_map	map;
+
   if (ft_strncmp(av[1] + ft_strlen(av[1]) - 4, ".cub", 4))
 		return  (err_ret("Error: Invalid file: extencion .cub;"));
-  (void)game;
+  game->map = &map;
+  game->img = &img;
   return  (0);
 }
 int	main(int ac, char **av)
