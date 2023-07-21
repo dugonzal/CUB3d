9f24d2a419530a34dacf2379aa79e55f101d055c
Duvan Gonzalez <dugonzal@c4r3s3.42urduliz.com>
2023-07-21 11:11:48 +0200

    ft_default

--- src/cub3d.c
+++ src/cub3d.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Dugonzal <dugonzal@student.42urduliz.com>  +#+  +:+       +#+        */
+/*   By: dugonzal <dugonzal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 13:11:47 by masla-la          #+#    #+#             */
-/*   Updated: 2023/07/20 13:18:52 by Dugonzal         ###   ########.fr       */
+/*   Updated: 2023/07/21 11:09:57 by dugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,17 @@ void	init_struct(t_game *lch)
 		return (err("malloc"));
 	}
 }
-
+void	ft_default(t_game *game)
+{
+	if (!game->img[0].path)
+		game->img[0].path = ft_strdup("img/wood3.xpm");
+	if (!game->img[1].path)
+		game->img[1].path = ft_strdup("img/0.xpm");
+	if (!game->img[2].path)
+		game->img[2].path = ft_strdup("img/1.xpm");
+	if (!game->img[3].path)
+		game->img[3].path = ft_strdup("img/2.xpm");
+}
 int	main(int ac, char **av)
 {
 	t_game	game;
@@ -39,6 +49,11 @@ int	main(int ac, char **av)
 	ft_bzero(&game, sizeof(t_game));
 	init_struct(&game);
 	parser (&game, av);
-	init_mlx(&game);
+	if (game.map->map)
+	{
+
+		ft_default(&game);
+		init_mlx(&game);
+	}
 	return (0);
 }
